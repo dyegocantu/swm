952a7a8bcef8bdf207aef8409a7bde7fe471f538
Dyego Cantu <c4ntuz@gmail.com>
2013-05-02 22:38:51 -0300

    with exception

--- arduino/arduino_interface.py
+++ arduino/arduino_interface.py
@@ -3,10 +3,11 @@
 
 import serial
 import time
+import glob
 
 class Arduino(object):
     '''Define a Arduino object.'''
-    
+
     def connect(self, port, baud_rate):
         self.arduino = serial.Serial(port, baud_rate)
         time.sleep(2)
@@ -16,10 +17,23 @@ class Arduino(object):
 
     def read(self):
         return self.arduino.readline()
-
+   
 if __name__ == '__main__':
+    
+    SERIAL_PORT = '/dev/ttyACM*'
+
     arduino = Arduino()
-    arduino.connect('/dev/ttyACM0',115200)
-    arduino.write('1')
-    print arduino.read()
+    available_ports = glob.glob(SERIAL_PORT)
+    if not available_ports:
+        print 'Arduino not found'
+        time.sleep(1)
+    else:
+        arduino_filename = available_ports[0]
+        try:
+            arduino.connect('/dev/ttyACM0', 115200)
+            arduino.write('1')
+            print arduino.read()
+        except serial.serialutil.SerialException:
+            print 'Serial exception'
+            time.sleep(1)
 
