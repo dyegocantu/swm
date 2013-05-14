d39e2bf3c116a694c2c0108b3522dff1655d5ae0
Dyego Cantu <c4ntuz@gmail.com>
2013-05-13 22:18:39 -0300

    arduino_interface created

---
+++ django/arduino_interface/arduino_interface.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from serial import Serial
+from serial.serialutil import SerialException
+from time import sleep
+from glob import glob
+#from models import ReadData
+
+class Arduino(object):
+    
+    def connect(self, port, baud_rate):
+        self.arduino = Serial(port, baud_rate)
+        sleep(2)
+
+    def write(self, data):
+        self.arduino.write(data)
+
+    def read(self):
+        return self.arduino.readline()
+
+
+if __name__ == '__main__':
+
+    SERIAL_PORT = '/dev/ttyACM0'
+    BAUD_RATE = 115200
+
+    arduino = Arduino()
+    arduino.connect(SERIAL_PORT, BAUD_RATE)
+    arduino.write('1')
+    print arduino.read()
+
