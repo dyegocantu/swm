64853d778d7d66600fb50f2a341cff5b45b1aac6
Dyego Cantu <c4ntuz@gmail.com>
2013-05-13 23:14:38 -0300

    changed the humidity line to first

--- django/arduino_interface/models.py
+++ django/arduino_interface/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ReadData(models.Model):
 
-    temperature = models.FloatField()
     humidity = models.FloatField()
+    temperature = models.FloatField()
     created = models.DateTimeField()
 
