a7af8cc616335d2d7bab692b87c3c7e30f59ed6d
Dyego Cantu <c4ntuz@gmail.com>
2013-05-14 21:57:47 -0300

    create the sensor app

---
+++ django/sensor/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class ReadData(models.Model):
+    
+    humidity = models.FloatField()
+    temperature = models.FloatField()
+    created = models.DateTimeField()
+
