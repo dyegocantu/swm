107ae430727b6b8b0d6eab14699b3044baf796ca
Dyego Cantu <c4ntuz@gmail.com>
2013-05-16 20:24:35 -0300

    line about crontab added

--- django/sensor.sh
+++ django/sensor.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# $ crontab -e
+# */1 * * * * /home/dyego/workspace/swm/django/sensor.sh
+
 source /home/dyego/.virtualenvs/swm/bin/activate
 cd /home/dyego/workspace/swm/django/
 /home/dyego/.virtualenvs/swm/bin/python /home/dyego/workspace/swm/django/arduino_interface.py
-
