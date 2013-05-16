b1a699c37a3cd97cccea4fedab38e650d7202dce
Dyego Cantu <c4ntuz@gmail.com>
2013-05-15 23:28:43 -0300

    created script to perform sensor

---
+++ django/sensor.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source /home/dyego/.virtualenvs/swm/bin/activate
+cd /home/dyego/workspace/swm/django/
+/home/dyego/.virtualenvs/swm/bin/python /home/dyego/workspace/swm/django/arduino_interface.py
+
