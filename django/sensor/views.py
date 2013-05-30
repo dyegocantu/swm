ccd4de357a94acb1083bd4eeeafa2d23209e2189
Dyego Cantu <engenharia_dyego@inobram.com.br>
2013-05-30 10:09:02 -0300

    returning only the most recent record

--- django/sensor/views.py
+++ django/sensor/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render_to_response
 from models import ReadData
 
 def data(request):
-    read_data_list = ReadData.objects.all()
-    return render_to_response('data.html', {'read_data_list': read_data_list})
+    read_data = ReadData.objects.latest('pk')
+    return render_to_response('data.html', {'read_data': read_data})
 
