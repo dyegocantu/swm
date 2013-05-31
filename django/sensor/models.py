8738ef307e35fbabba5e11c3a71b7c450f6f6e38
Dyego Cantu <engenharia_dyego@inobram.com.br>
2013-05-31 16:46:29 -0300

    added the unicode function of readdata model

--- django/sensor/models.py
+++ django/sensor/models.py
@@ -3,8 +3,11 @@
 from django.db import models
 
 class ReadData(models.Model):
-    
     humidity = models.FloatField()
     temperature = models.FloatField()
     created = models.DateTimeField()
+
+    def __unicode__(self):
+        return u'ReadData(%f, %f, %s)' % \
+                (self.humidity, self.temperature, self.created)
 
