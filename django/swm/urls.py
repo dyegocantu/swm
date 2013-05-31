ac78e445439461c5aebd5025664ee65860958013
Dyego Cantu <c4ntuz@gmail.com>
2013-05-30 23:50:41 -0300

    add media path in urls.py

--- django/swm/urls.py
+++ django/swm/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -20,3 +21,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', \
+                {'document_root': settings.MEDIA_ROOT}),
+)
+
