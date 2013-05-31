d7ad2d0790d1e04a40203f06a9bfc203fdab0a56
Dyego Cantu <c4ntuz@gmail.com>
2013-05-30 23:44:06 -0300

    added media path

--- django/swm/settings.py
+++ django/swm/settings.py
@@ -55,12 +55,14 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(ROOTDIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
+
+#ADMIN_MEDIA_PREFIX = '/admin_media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
