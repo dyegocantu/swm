5bf28f102bb3606c1e0c8e8551205dedc71c4122
Dyego Cantu <engenharia_dyego@inobram.com.br>
2013-06-10 13:35:00 -0300

    added jquery

--- django/swm/static/js/jquery.js
+++ django/swm/static/js/jquery.js
@@ -15,7 +15,7 @@
 
 // Can't do this because several apps including ASP.NET trace
 // the stack via arguments.caller.callee and Firefox dies if
-// you try to trace through "use strict" call chains. (#13335)
+// you try to trace through "use strict" call chains.(#13335)
 // Support: Firefox 18+
 //"use strict";
 var
