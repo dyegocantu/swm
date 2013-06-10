38cdefb8c841818b4e2a50a73cd7960a58358971
Dyego Cantu <c4ntuz@gmail.com>
2013-06-10 12:21:28 -0300

    modified templates

--- django/swm/settings.py
+++ django/swm/settings.py
@@ -76,7 +76,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '//localhost:8000/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
